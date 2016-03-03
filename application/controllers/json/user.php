6106fd7b333867b1968a76074d235fb545954bb4
yiguid <yiguid@gmail.com>
2016-03-03 18:40:13 +0800

    init

--- application/controllers/json/user.php
+++ application/controllers/json/user.php
@@ -7,5 +7,45 @@ class User extends CI_Controller {
 		$this->load->model('json_user_mdl');
 		header('content-type:application/json;charset=utf8');  
 	}
+
+	public function donation($page = 1,$num_per_page = 10)
+	{
+		$id = 15;
+		$user_list = $this->json_user_mdl->donation_get($id,$page,$num_per_page);	
+		echo "{\"user\":".$this->json_unescaped_unicode(json_encode($user_list))."}";
+	}
+
+	public function donation_zhongchou($page = 1,$num_per_page = 10)
+	{
+		$id = 11;
+		$user_list = $this->json_user_mdl->donation_zhongchou_get($id,$page,$num_per_page);	
+		echo "{\"user\":".$this->json_unescaped_unicode(json_encode($user_list))."}";
+	}
+
+	public function activity($page = 1,$num_per_page = 10)
+	{
+		$id = 15;
+		$user_list = $this->json_user_mdl->activity_get($id,$page,$num_per_page);	
+		echo "{\"user\":".$this->json_unescaped_unicode(json_encode($user_list))."}";
+	}
+
+	public function volunteer($page = 1,$num_per_page = 10)
+	{		
+		$id = 15;	
+		$user_list = $this->json_user_mdl->volunteer_get($id,$page,$num_per_page);	
+		echo "{\"user\":".$this->json_unescaped_unicode(json_encode($user_list))."}";	
+	}
+
+	private function json_unescaped_unicode($str)
+	{
+		return preg_replace_callback(
+	            "#\\\u([0-9a-f]{4})#i",
+	            function($matchs)
+	            {
+	                 return iconv('UCS-2BE', 'UTF-8', pack('H4', $matchs[1]));
+	            },
+	             $str
+	            );
+	}
 }
 ?>
--- application/models/json_user_mdl.php
+++ application/models/json_user_mdl.php
@@ -6,5 +6,52 @@ class Json_user_mdl extends CI_Model {
 	{
 		parent:: __construct();
 	}
+//$headers = $this->input->request_headers();return sha1(md5($username.$password.microtime()));
+	public function donation_get($id, $page, $num_per_page)
+	{
+		$this->db->select('donation_order.ordertime,donation_order.total,donation_order.status,temple.name');
+		$this->db->from('donation_order');	
+		$this->db->join('temple',"temple.id = donation_order.templeid");
+		$this->db->join('user',"user.username = donation_order.username");
+		$this->db->limit($num_per_page,($page - 1) * $num_per_page);
+		$this->db->order_by('ordertime','desc');	
+		$this->db->where('user.id',$id);			
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	public function donation_zhongchou_get($id, $page, $num_per_page)
+	{
+		$this->db->select('zhongchou_reward.money,zhongchou_reward.award,zhongchou_record.recordtime,zhongchou_record.status,zhongchou_record.rewardstatus');	
+		$this->db->from('zhongchou_record');		
+		$this->db->join('zhongchou_reward',"zhongchou_reward.id = zhongchou_record.rewardid");
+		$this->db->limit($num_per_page,($page - 1) * $num_per_page);		
+		$this->db->order_by('recordtime','desc');	
+		$this->db->where('zhongchou_record.userid',$id);		
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	public function activity_get($id, $page, $num_per_page)
+	{
+		$this->db->select('title');
+		$this->db->from('activity');
+		$this->db->join('activity_register',"activity_register.activityid = activity.id");	
+		$this->db->limit($num_per_page,($page - 1) * $num_per_page);	
+		$this->db->where('activity_register.userid',$id);		        
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	public function volunteer_get($id, $page, $num_per_page)
+	{
+		$this->db->select('title');
+		$this->db->from('volunteer');
+		$this->db->join('volunteer_register',"volunteer_register.volunteerid = volunteer.id");	
+		$this->db->limit($num_per_page,($page - 1) * $num_per_page);	
+		$this->db->where('volunteer_register.userid',$id);		        
+		$query = $this->db->get();
+		return $query->result();
+	}
 }
 ?>
